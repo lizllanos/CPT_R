6be44591da4526fda4e32ee9c8b5f3b22c327679
lizllanos <llanoslizeth@gmail.com>
2017-11-02 16:28:54 -0500

    Script graphs for CFSv2

---
+++ 04_salidas_CPT_CFSv2.R
@@ -0,0 +1,45 @@
+# Created by: Diego Agudelo
+# Modified by: Lizeth Llanos
+# Description: This script creates graphics for the output from CPT_R runs with CfsV2
+
+if(require(ggplot2)==FALSE){install.packages("ggplot2")}
+library("ggplot2")
+
+path <- "C:/Users/lllanos/Desktop/cpt_batch/output" 
+
+all_path <- list.files(path,pattern = "index",full.names = T)
+
+name_files <- basename(all_path)
+names_month <- as.numeric(unlist(lapply(strsplit(name_files,"_"),"[",1)))
+lead <- function(x){ 
+  cat(paste0(x,"\n"))
+  month=as.numeric(unlist(lapply(strsplit(x,"_"),"[",1)))
+ 
+    if(substr(x,4,6) %in% month.abb){
+    l=substr(x,4,6)
+  }else{
+    l=substr(x,3,5)
+  }
+  if((month-4)<=0) pos =(month-4)+12  else pos =(month-4)
+  if(month.abb[pos]==l) out <- "LT-3"
+  else if(month.abb[month-1]==l)out <- "LT-0"
+  else out <- "LT-5"
+  return(out)
+  
+}
+lead_times<- sapply(name_files,lead,USE.NAMES = F)
+
+data <- lapply(all_path,function(x)read.table(x,skip=3,dec=".",fill=TRUE,stringsAsFactors=FALSE))
+pos_good <- lapply(data,function(x)c(which(x[,1]=="Training")-1,dim(x)[1]))
+value_good <- Map(function(x,y)as.numeric(x[y,8]),data,pos_good)
+ 
+data_combi <- Map(function(x,y,z) cbind.data.frame(trimester=z,lead=x,goodness=y),lead_times,value_good,names_month)
+data_final <- do.call(what = "rbind",args =data_combi)
+data_final$lead=factor(data_final$lead,levels(data_final$lead)[c(3,1,2)])
+
+box <- ggplot(data_final, aes(as.factor(trimester), goodness, fill = lead))
+box <- box + scale_x_discrete( labels = c("MAM","AMJ","ASO","DEF"))
+box <- box + geom_boxplot( width=0.6, position = position_dodge(width = 0.7))
+box <- box + labs(y="Goodness index", x="Season",fill="Lead Time")
+box
+
